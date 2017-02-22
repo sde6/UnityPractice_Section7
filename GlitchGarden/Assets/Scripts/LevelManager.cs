7057a06f58d92c0579049d63683197cb6bee6045
sde6 <sde6@case.edu>
2017-02-22 13:18:17 -0800

    Splash Screen
    Splash screen loads image, 2 second delay, then start scene is loaded

--- GlitchGarden/Assets/Scripts/LevelManager.cs
+++ GlitchGarden/Assets/Scripts/LevelManager.cs
@@ -2,7 +2,13 @@
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
-
+	
+	public float autoLoadNextLevelAfter;
+	
+	void Start(){
+		Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+	
 	public void LoadLevel(string name){
 		Debug.Log ("New Level load: " + name);
 		Application.LoadLevel (name);
@@ -11,6 +17,10 @@ public class LevelManager : MonoBehaviour {
 	public void QuitRequest(){
 		Debug.Log ("Quit requested");
 		Application.Quit ();
+	}
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel + 1);
 	}
 
 }
